d0af1b29981761be0191d43a18a6daa7c69b4f2a
unknown <luca.botta44@gmail.com>
2022-07-01 01:33:28 +0200

    Add button in PfManager Table

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,5 +1,5 @@
 var Border = false;
-//loadTable1();
+loadTable1();
 
 //Function to set active to the active <li> in sidebar
 function navActive(num){
@@ -36,14 +36,134 @@ async function loadTable1(){
 	var tbody = document.getElementById("tbody1");
 	for(var i = 0; i < portFolios.length; i++) {
 		var tr = document.createElement("tr");
+		tr.className = "itemTabella";
+
 		var td1 = document.createElement("td");
+		td1.className = "itemTd"
 		td1.appendChild(document.createTextNode(portFolios[i].pfName));
 		tr.appendChild(td1);
+
 		var td2 = document.createElement("td");
+		td2.className = "itemTd"
 		td2.appendChild(document.createTextNode(portFolios[i].tickers));
 		tr.appendChild(td2);
+
+		var td3 = document.createElement("td");
+		td3.className = "modifyTd"
+		var button = document.createElement("button");
+		//button.appendChild(document.createTextNode("Modify"));
+		button.id = "modifyButt" + i;
+        button.className = "btn btn-dark modifyPf";
+        button.type = "button";
+		button.dataset.toggle = "modal";
+		button.dataset.target = "#modifyModal" + i;
+		button.onclick = function() { modifyPfManager(this); };
+		var modifyImg = document.createElement("img");
+		modifyImg.id = "modifyImg";
+		modifyImg.src = "img/modify-icon.jpg";
+		button.append(modifyImg);
+        td3.appendChild(button);
+		
+		//Modal
+		var modalDiv1 = document.createElement("div");
+		modalDiv1.className = "modal fade";
+		modalDiv1.id = "modifyModal" + i;
+		modalDiv1.tabIndex = "-1";
+		modalDiv1.role = "dialog";
+		modalDiv1.ariaLabelledBy = "modifyModalLabel";
+		modalDiv1.ariaHidden = "true";
+
+		var modalDiv2 = document.createElement("div");
+		modalDiv2.className = "modal-dialog";
+		modalDiv2.role = "document";
+
+		var modalDiv3 = document.createElement("div");
+		modalDiv3.className = "modal-content";
+
+		var modalDiv4 = document.createElement("div");
+		modalDiv4.className = "modal-header";
+
+		//Modal title
+		var modalTitle = document.createElement("h4");
+		modalTitle.className = "modal-title";
+		modalTitle.id = "modifyModalLabel";
+		modalTitle.appendChild(document.createTextNode("Modify Portfolio"));
+		modalDiv4.appendChild(modalTitle);
+
+		var closeButton = document.createElement("button");
+		closeButton.type = "button";
+		closeButton.className = "close";
+		closeButton.dataset.dismiss = "modal";
+		closeButton.ariaLabel = "Close";
+
+		var closeSpan = document.createElement("span");
+		closeSpan.innerHTML = "&times;";
+		closeButton.appendChild(closeSpan);
+		modalDiv4.appendChild(closeButton);
+		modalDiv3.appendChild(modalDiv4);
+
+		//Modal content
+		var modalDiv5 = document.createElement("div");
+		modalDiv5.className = "modal-body";
+
+		var pfName = document.createElement("h5");
+		pfName.appendChild(document.createTextNode("Portfolio Name: "));
+		var pfNameInput = document.createElement("input");
+		pfNameInput.value = portFolios[i].pfName;
+		pfName.appendChild(pfNameInput);
+		modalDiv5.appendChild(pfName);
+
+		var pfTickers = document.createElement("h5");
+		pfTickers.appendChild(document.createTextNode("Portfolio Tickers: "));
+		var pfTickersInput = document.createElement("input");
+		pfTickersInput.value = portFolios[i].tickers;
+		pfTickers.appendChild(pfTickersInput);
+		modalDiv5.appendChild(pfTickers);
+
+		modalDiv3.appendChild(modalDiv5);
+
+		var modalDiv6 = document.createElement("div");
+		modalDiv6.className = "modal-footer";
+
+		var footCloseButton = document.createElement("button");
+		footCloseButton.type="button";
+		footCloseButton.className = "btn btn-secondary";
+		footCloseButton.dataset.dismiss = "modal";
+		footCloseButton.appendChild(document.createTextNode("Close"));
+		modalDiv6.appendChild(footCloseButton);
+
+		var saveButton = document.createElement("button");
+		saveButton.type = "button";
+		saveButton.className = "btn btn-primary";
+		saveButton.appendChild(document.createTextNode("Save changes"));
+		modalDiv6.appendChild(saveButton);
+
+		modalDiv3.appendChild(modalDiv6);
+		modalDiv2.appendChild(modalDiv3);
+		modalDiv1.appendChild(modalDiv2);
+
+		td3.appendChild(modalDiv1);
+		
+		tr.appendChild(td3);
 		tbody.insertBefore( tr, tbody.lastElementChild);
 	}
+}
+
+function modifyPfManager(item){
+	pfNum = item.id.toString();
+	pfNum = pfNum.split("modifyButt")[1]
+	console.log(pfNum);
+}
+
+function addPfInputTickers(input){
+
+	if(input.value.slice(-1) == ","){
+		document.getElementById(input.id).setAttribute("disabled", true);
+		document.getElementById("loaderIcon").style.display = "block";
+		var ticker = input.value.split(",")[0];
+		console.log(ticker); 
+	}
+	
 }
 
 function showBorder(){
--- py/main.py
+++ py/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import asyncio
 from pyodide.http import pyfetch
+#import mysql.connector
 
 async def loadJson():
     response = await pyfetch(url="./json/portfolios.json", method="GET")
@@ -15,17 +16,29 @@ async def loadJson():
 
 async def fillTablePfManager():
     pfList = await loadJson()
-    tbody = document.getElementById("tbody1");
+    tbody = document.getElementById("tbody1")
     i = 0
     for pf in pfList["PortFolios"]:
-        tr = document.createElement("tr");
-        td1 = document.createElement("td");
-        td1.appendChild(document.createTextNode(str(pf["pfName"])));
-        tr.appendChild(td1);
-        td2 = document.createElement("td");
-        td2.appendChild(document.createTextNode(str(pf["tickers"])));
-        tr.appendChild(td2);
+        tr = document.createElement("tr")
+        td1 = document.createElement("td")
+        td1.appendChild(document.createTextNode(str(pf["pfName"])))
+        tr.appendChild(td1)
+        td2 = document.createElement("td")
+        td2.appendChild(document.createTextNode(str(pf["tickers"])))
+        tr.appendChild(td2)
+
+        td3 = document.createElement("td")
+        button = document.createElement("button")
+        button.appendChild(document.createTextNode("Modify"))
+        button.id = "modifyPf"
+        button.className = "btn btn-dark"
+        button.type = "button"
+        td3.appendChild(button)
+        
+        tr.appendChild(td3)
+
+        
         tbody.insertBefore( tr, tbody.lastElementChild );
         i += 1
 
-fillTablePfManager()
+#fillTablePfManager()

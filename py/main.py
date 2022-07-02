4382733ad97242268e6e8533e13c4947004dc3ef
unknown <luca.botta44@gmail.com>
2022-07-02 14:45:48 +0200

    Add AlphaVantage API

--- py/main.py
+++ py/main.py
@@ -6,7 +6,7 @@ from js import document
 from pyodide import create_proxy
 from pandas_datareader import data as wb
 from pyodide.http import pyfetch
-import PyOpenSSL
+import requests
 #import mysql.connector
 
 async def loadJson():
@@ -31,7 +31,32 @@ def checkTickerYahoo(event):
     #data = wb.DataReader(ticker, data_source="yahoo")
     #console.log(data)
 
-inputTickerProxy = create_proxy(checkTickerYahoo)
+async def checkTickerYahoo1(event):
+    text = document.getElementById("pfInputTicker").value
+    if(text[-1] == ","):
+        document.getElementById("pfInputTicker").setAttribute("disabled", True)
+        document.getElementById("loaderIcon").style.display = "block"
+        ticker = text.split(",")[0]
+        apiKey = "GBD72Q8YTW5QIDIN"
+        url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + ticker + '&apikey=' + apiKey
+        response = await pyfetch(
+            url,
+            method="GET"
+        )
+        out = await response.json()
+        try:        
+            tickerName = out["Name"]
+            console.log(tickerName)
+        except:
+            console.log("Ticker not found")
+            document.getElementById("pfInputTicker").removeAttribute("disabled")
+            document.getElementById("loaderIcon").style.display = "none"
+            document.getElementById("pfInputTicker").value = ""
+        
+    #data = wb.DataReader(ticker, data_source="yahoo")
+    #console.log(data)
+
+inputTickerProxy = create_proxy(checkTickerYahoo1)
 inputTicker = document.getElementById("pfInputTicker")
 inputTicker.addEventListener("input", inputTickerProxy)
 
